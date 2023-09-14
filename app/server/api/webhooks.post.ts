dc0c9508d6f77f7a6fb67ebfd7331d59bb799e64
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-09-14 16:25:25 +0200

    feat: query types

--- app/server/api/webhooks.post.ts
+++ app/server/api/webhooks.post.ts
@@ -6,12 +6,62 @@ export default defineEventHandler(async (event) => {
 
   // Get commit from payload
   const commit = payload.event.data.commit;
-  const commitObjectId = commit.objectId;
+  const objectId = commit.objectId;
+  const streamId = commit.streamId;
 
   // Get assets from commit
-  GqlTest();
+  const data = await GqlGetFurnitures({
+    objectId,
+    streamId,
+    myQuery: [
+      {
+        field: "speckle_type",
+        value:
+          "Objects.BuiltElements.Revit.RevitElementType:Objects.BuiltElements.Revit.RevitSymbolElementType",
+        operator: "=",
+      },
+    ],
+  });
 
-  // Update assets database
+  console.log(data.stream?.object?.children.objects.length);
+  data.stream?.object?.children.objects.forEach((obj) => {
+    const typeId = console.log("Type ID", obj?.data["id"]);
+    const familyName = console.log("Family", obj?.data["family"]);
+    const typeName = console.log("Type", obj?.data["type"]);
+    const category = console.log("Category", obj?.data["category"]);
+  });
+
+  // Create assets in database
+  interface Asset {
+    id: string;
+    name: string;
+    cost?: number | null;
+    category: string;
+  }
+
+  const assets =
+    data.stream?.object?.children.objects.map<Asset>((obj) => {
+      const id = obj?.data["id"];
+      const familyName = obj?.data["family"];
+      const typeName = obj?.data["type"];
+      const category = obj?.data["category"];
+
+      let assetName: string =
+        familyName === typeName ? familyName : familyName + ":" + typeName;
+
+      const asset: Asset = { id, name: assetName, category };
+
+      return asset;
+    }) || [];
+
+  const queryTotalCount = data.stream?.object?.children.totalCount;
+  const assetsCount = assets?.length;
+  if (queryTotalCount !== assetsCount) {
+    console.warn(
+      "Query total count and assets parsed count do not match, implement pagination query more."
+    );
+  }
+
   return {
     message: "success",
   };
